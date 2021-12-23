b08e3cda19da8d9993b7ec11b88daf14823cfd89
Exhilarate <vitaly.vepsha@gmail.com>
2021-12-23 14:15:36 +0200

    clear

--- app/app.js
+++ app/app.js
@@ -22,13 +22,6 @@ require('bootstrap-sass');
 // Modules
 var Forms = require('_modules/forms');
 var Slider = require('_modules/slider');
-var Popup = require('_modules/popup');
-var Fancy_select = require('_modules/fancyselect');
-var Jscrollpane = require('_modules/jscrollpane');
-var LightGallery = require('_modules/lightgallery');
-var Jslider = require('_modules/jslider');
-var Fancybox = require('_modules/fancybox');
-require('_modules/succinct/succinct');
 require('../node_modules/sumoselect/jquery.sumoselect.min.js');
 
 // Stylesheet entrypoint
@@ -37,15 +30,10 @@ require('_stylesheets/app.scss');
 // Are you ready?
 $(function() {
   new Forms();
-  new Popup();
-  new Fancy_select();
-  new Jscrollpane();
-  new LightGallery();
   new Slider();
-  new Jslider();
-  new Fancybox();
 
-    // fixed header
+  // fixed header
+
   var header = $('.header'),
     scrollPrev = 0;
 
@@ -59,7 +47,7 @@ $(function() {
     scrollPrev = scrolled;
   });
 
-    // languages
+  // languages
 
   $('.header-lang__curr').click(function() {
     $(this).closest('.header-lang').toggleClass('active');
@@ -77,7 +65,7 @@ $(function() {
     e.stopPropagation();
   });
 
-    // mobile menu
+  // mobile menu
 
   var touch = $('.mobile-menu__btn');
 
@@ -109,20 +97,21 @@ $(function() {
     e.stopPropagation();
   });
 
-    // faq
+  // faq
 
   $('.faq-main__header').click(function() {
     $(this).closest('.faq-main__item').find('.faq-main__body').slideToggle()
             .closest('.faq-main__item').toggleClass('active').siblings().removeClass('active').find('.faq-main__body').slideUp();
   });
 
-    // select
+  // select
 
   $('.select').SumoSelect({
     forceCustomRendering: true
   });
 
-    // show/hide password
+  // show/hide password
+
   $('#show-password').on('click', function() {
     if (!$(this).hasClass('active')) {
       $(this).addClass('active').next('label').html('Hide password').closest('form').find('input[name="password"]').attr('type', 'text');
@@ -131,83 +120,4 @@ $(function() {
       $(this).removeClass('active').next('label').html('Show password').closest('form').find('input[name="password"]').attr('type', 'password');
     }
   });
-
-  $('.video__inner').on('click', function(e) {
-    $(this).find('img').hide();
-    $(this).find('svg').hide();
-    $(this).find('iframe').show();
-    $(this).find('iframe')[0].src += '?autoplay=1';
-    e.preventDefault();
-  });
-
-    // faq
-  if ($('.acc')) {
-    $('.acc .acc__content').css('display', 'none');
-    $('.acc .acc__title').click(function() {
-      $(this).toggleClass('active').next().slideToggle();
-      $('.acc .acc__title').not(this).removeClass('active').next().slideUp();
-    });
-  }
-
-    // crop text
-
-  if ($('.werden-txt').length) {
-    $('.werden-txt').each(function() {
-      var text = $(this).text();
-      if ($(this).text().replace(/\s+/g, '').length > 320) {
-        $(this).succinct({
-          size: 320,
-          ignore: false
-        });
-        $('.werden__text-more').show();
-      }
-      else {
-        $('.werden__text-more').hide();
-      }
-      $('.werden__text-more').click(function() {
-        $(this).hide();
-        $('.werden-txt').html(text);
-      });
-    });
-  }
-
-  if ($('.sie-item__text').length) {
-    $('.sie-item__text').each(function() {
-      var text = $(this).text();
-      if ($(this).text().replace(/\s+/g, '').length > 110) {
-        $(this).succinct({
-          size: 110,
-          ignore: false
-        });
-        $(this).next('.sie-btn__more').show();
-      }
-      else {
-        $(this).next('.sie-btn__more').hide();
-      }
-      $(this).next('.sie-btn__more').click(function() {
-        $(this).hide();
-        $(this).closest('.sie-item__inner').find('.sie-item__text').html(text);
-      });
-    });
-  }
-
-  if ($('.produkts-slide__text').length) {
-    $('.produkts-slide__text').each(function() {
-      var text = $(this).text();
-      if ($(this).text().replace(/\s+/g, '').length > 110) {
-        $(this).succinct({
-          size: 110,
-          ignore: false
-        });
-        $(this).next('.produkts-slide__more').show();
-      }
-      else {
-        $(this).next('.produkts-slide__more').hide();
-      }
-      $(this).next('.produkts-slide__more').click(function() {
-        $(this).hide();
-        $(this).closest('.produkts-slide__about').find('.produkts-slide__text').html(text);
-      });
-    });
-  }
 });
